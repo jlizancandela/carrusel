1e15f4a17bdbe736e316f76f26e59568e1abeddf
jorge <jlizancandela@gmail.com>
2024-04-03 15:04:37 +0200

    agregado contenedor texto

--- src/components/carrusel.tsx
+++ src/components/carrusel.tsx
@@ -1,36 +1,18 @@
 import { useEffect, useState } from "react";
-import styled from "styled-components";
+import "../components/carrusel.css";
+interface CarruselProps {
+  images: {
+    src: string;
+    alt: string;
+  }[];
+}
 
-type Image = {
-  src: string;
-  alt: string;
-};
-
-export function Carrusel(props: { images: Image[] }) {
+export function Carrusel(props: CarruselProps) {
   const cuantas = props.images.length - 1;
   const [puntero, setPuntero] = useState(1);
   const [src, setSrc] = useState(props.images[0].src);
   const [alt, setAlt] = useState(props.images[0].alt);
   const [loaded, setLoaded] = useState(false);
-  const Carrusel = styled.section`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    height: 100vh;
-  `;
-  console.log(cuantas);
-  console.log(props.images[puntero]);
-  const SliderImg = styled.img`
-    width: 75%;
-    aspect-ratio: 16/9;
-    object-fit: cover;
-    margin: auto;
-    opacity: 0;
-    transition: 3s;
-    &.loaded {
-      opacity: 1;
-    }
-  `;
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -39,15 +21,15 @@ export function Carrusel(props: { images: Image[] }) {
         setPuntero(puntero >= cuantas ? 0 : puntero + 1);
         setSrc(props.images[puntero].src);
         setAlt(props.images[puntero].alt);
-      }, 500);
+      }, 1000);
     }, 5000);
     return () => clearInterval(interval);
   });
 
   return (
     <>
-      <Carrusel>
-        <SliderImg
+      <section>
+        <img
           src={src}
           alt={alt}
           className={loaded ? "loaded" : "unloaded"}
@@ -55,7 +37,8 @@ export function Carrusel(props: { images: Image[] }) {
             setLoaded(true);
           }}
         />
-      </Carrusel>
+        <h1>{alt}</h1>
+      </section>
     </>
   );
 }
